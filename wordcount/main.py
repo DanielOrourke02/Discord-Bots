2911ef0ed9412560911eca525a64d9187b59881a
Daniel O'Rourke <145261080+DanielOrourke02@users.noreply.github.com>
2024-08-02 20:59:13 +0100

    Update main.py
    fixed bugs, updated

--- wordcount/main.py
+++ wordcount/main.py
@@ -1,23 +1,14 @@
 # PROGRAMMED BY mal023
 
-# auto install modules
-try:
-    from discord.ext import commands
-    import discord
-except ModuleNotFoundError:
-    import os
-    os.system('pip install git+https://github.com/Rapptz/discord.py')
-    
-from discord.ext import commands
-from discord import app_commands
+from discord.ext.pages import Paginator
+from discord import Option
 import discord
 import json
 import time
 
 
-prefix = '.' # ignore this the prefix is slash commands anyway
-bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
-
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
 
 # Load the existing word count data from the JSON file, if it exists
 try:
@@ -26,10 +17,7 @@ try:
 except FileNotFoundError:
     wordcount_data = {}
 
-
 # Load the config
-
-# Load the config file
 with open('config.json', 'r') as file:
     config = json.load(file)
 
@@ -67,58 +55,42 @@ async def on_ready():
             guild_count = guild_count + 1
 
         print(f"{bot.user.display_name} is in " + str(guild_count) + " guilds.\n")
-
-        synced = await bot.tree.sync() # Loads/syncs commands
-        print(f"Synced {len(synced)} command(s)")
-
-        # listing
+        
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=status))
-
-        # watching, unhash the line bellow. 
-        #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))
-
-        # playing, unhash the line bellow. 
-        #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=status))
-
-        # streaming, unhash the line bellow. 
-        #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name=status))
-
-
     except Exception as e:
         print(e)
 
 
 # Simple Test command
-@bot.tree.command(name="ping")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message('Pong! {0}'.format(round(bot.latency, 1)), ephemeral=True)
+@bot.slash_command(name="ping", description="Ping the bot")
+async def ping(ctx):
+    await ctx.respond('Pong! {0}'.format(round(bot.latency, 1)), ephemeral=True)
 
 
-@bot.tree.command(name="help")
-async def help_command(interaction: discord.Interaction):
-    # Define the list of commands with descriptions
-    commands_list = [
-        {"name": "wordcount", "description": "Set your current word count"},
-        {"name": "servertotal", "description": "The total server word count"},
-        {"name": "leaderboard", "description": "List the top 10 users with the highest word count"},
-        {"name": "getwordc", "description": "Get a user's word count"},
-        {"name": "reset", "description": "Clear wordcount.json (admin only)"},
-        {"name": "setwordc", "description": "Set a user's word count (admin only)"},
-        # Add more commands as needed with their descriptions
+@bot.slash_command(name="help", description="Lists all commands")
+async def help_command(ctx: discord.ApplicationContext):
+    # Get the list of command names and descriptions
+    command_info = [(command.name, command.description) for command in bot.commands]
+    # Split the commands into chunks of 15
+    chunks = [command_info[i:i + 15] for i in range(0, len(command_info), 15)]
+    # Create pages with the command names and descriptions
+    pages_list = [
+        discord.Embed(
+            title="List of commands:",
+            description="\n".join([f"`/{name}` - {description}" for name, description in chunk]),
+            color=discord.Color.blurple()
+        ) for chunk in chunks
     ]
 
-    # Construct the help message as an embed
-    embed = discord.Embed(title="Bot Commands", color=discord.Colour.blue())
-    for command in commands_list:
-        embed.add_field(name=f"/{command['name']}", value=command['description'], inline=False)
-    embed.set_footer(text=f"Use /help <command> for more information on a command.")
+    # Create the paginator
+    paginator = Paginator(pages=pages_list, loop_pages=True)
 
-    # Send the help message as an ephemeral response
-    await interaction.response.send_message(embed=embed)
+    # Respond with the paginated message
+    await paginator.respond(ctx.interaction, ephemeral=False)
 
 
-@bot.tree.command(name="wordcount")
-async def wordcount_command(interaction: discord.Interaction, count: int):
+@bot.slash_command(name="wordcount", description="Set your current word count")
+async def wordcount(ctx: discord.ApplicationContext, count: Option(int, description="Set your wordcount", required=True)): # type: ignore
     # Load word count data
     wordcount_data = load_wordcount_data()
 
@@ -131,7 +103,7 @@ async def wordcount_command(interaction: discord.Interaction, count: int):
         )
     else:
         # Update user's word count
-        user_id = str(interaction.user.id)
+        user_id = str(ctx.user.id)
         current_count = wordcount_data.get(user_id, 0)  # Get current user's word count, default to 0 if not found
         wordcount_data[user_id] = current_count + count  # Add the new count to the current count
 
@@ -149,17 +121,15 @@ async def wordcount_command(interaction: discord.Interaction, count: int):
             title="Word Count Recorded",
             description=f"Your word count has been recorded as {count}.\n"
                         f"Your total word count is {user_total}.\n",
-            color=discord.Colour.blue()
+            color=discord.Color.blurple()
         )
-        
-    embed.set_footer(text=f"Made by mal023")
 
     # Send the confirmation message as an ephemeral response
-    await interaction.response.send_message(embed=confirmation_embed)
+    await ctx.respond(embed=confirmation_embed)
 
 
-@bot.tree.command(name="servertotal")
-async def servertotal_command(interaction: discord.Interaction):
+@bot.slash_command(name="servertotal", description="The total server word count")
+async def servertotal_command(ctx: discord.ApplicationContext):
     # Load word count data
     wordcount_data = load_wordcount_data()
 
@@ -170,17 +140,15 @@ async def servertotal_command(interaction: discord.Interaction):
     embed = discord.Embed(
         title="Server Total Word Count",
         description=f"The total word count for the entire server is {server_total}.",
-        color=discord.Colour.blue()
+        color=discord.Colour.blurple()
     )
 
-    embed.set_footer(text=f"Made by mal023")
-
     # Send the embed message
-    await interaction.response.send_message(embed=embed)
+    await ctx.respond(embed=embed)
 
 
-@bot.tree.command(name="leaderboard")
-async def leaderboard_command(interaction: discord.Interaction):
+@bot.slash_command(name="leaderboard", description="List the top 10 users with the highest word count")
+async def leaderboard_command(ctx: discord.ApplicationContext):
     # Load the word count data from the JSON file
     with open('wordcount.json', 'r') as file:
         wordcount_data = json.load(file)
@@ -194,7 +162,7 @@ async def leaderboard_command(interaction: discord.Interaction):
     # Construct the leaderboard message as an embed with blue color
     leaderboard_embed = discord.Embed(
         title="Top 10 Users with Highest Word Count",
-        color=discord.Colour.blue()
+        color=discord.Colour.blurple()
     )
     for index, (user_id, word_count) in enumerate(top_10, start=1):
         user = bot.get_user(int(user_id))
@@ -202,22 +170,20 @@ async def leaderboard_command(interaction: discord.Interaction):
             leaderboard_embed.add_field(name=f"{index}. {user.name}", value=f"{word_count} words", inline=False)
         else:
             leaderboard_embed.add_field(name=f"{index}. Unknown User ({user_id})", value=f"{word_count} words", inline=False)
-            
-    embed.set_footer(text=f"Made by mal023")
 
     # Send the leaderboard message as an ephemeral response
-    await interaction.response.send_message(embed=leaderboard_embed)
+    await ctx.respond(embed=leaderboard_embed)
 
 
-@bot.tree.command(name="getwordc")
-async def getwordc_command(interaction: discord.Interaction, user: discord.User = None):
+@bot.slash_command(name="getwordc", description="Get a user's word count")
+async def getwordc_command(ctx: discord.ApplicationContext, user: discord.User=None):
     # Load the word count data from the JSON file
     with open('wordcount.json', 'r') as file:
         wordcount_data = json.load(file)
 
     # If no user is mentioned, use the user who invoked the command
     if user is None:
-        user = interaction.user
+        user = ctx.user
 
     # Get the user's ID
     user_id = str(user.id)
@@ -236,22 +202,20 @@ async def getwordc_command(interaction: discord.Interaction, user: discord.User 
     wordcount_embed = discord.Embed(
         title="Word Count",
         description=message,
-        color=discord.Colour.blue()
+        color=discord.Colour.blurple()
     )
 
-    embed.set_footer(text=f"Made by mal023")
-
     # Send the message with the user's word count as an ephemeral response
-    await interaction.response.send_message(embed=wordcount_embed)
+    await ctx.respond(embed=wordcount_embed)
 
 
 # ----------------------------------------------ADMIN COMMANDS----------------------------------------------
 
 
-@bot.tree.command(name="reset")
-async def reset(interaction: discord.Interaction):
+@bot.slash_command(name="reset", description="Clear wordcount.json (admin only)")
+async def reset(ctx: discord.ApplicationContext):
     # Check if the user invoking the command has Administrator permission
-    if not interaction.user.guild_permissions.administrator:
+    if not ctx.user.guild_permissions.administrator:
         message = "You do not have permission to use this command."
     else:
         message = "Please run `/reset_confirm` to reset the global wordcount!"
@@ -260,19 +224,17 @@ async def reset(interaction: discord.Interaction):
     reset_embed = discord.Embed(
         title="Reset Confirmation",
         description=message,
-        color=discord.Colour.blue()
+        color=discord.Colour.blurple()
     )
-    
-    embed.set_footer(text=f"Made by mal023")
 
     # Send the message as an ephemeral response
-    await interaction.response.send_message(embed=reset_embed, ephemeral=True)
+    await ctx.respond(embed=reset_embed, ephemeral=True)
 
 
-@bot.tree.command(name="reset_confirm")
-async def reset_confirm(interaction: discord.Interaction):
+@bot.slash_command(name="reset_confirm", description="Confirm the reset of wordcount.json (admin only)")
+async def reset_confirm(ctx: discord.ApplicationContext):
     # Check if the user invoking the command has Administrator permission
-    if not interaction.user.guild_permissions.administrator:
+    if not ctx.user.guild_permissions.administrator:
         message = "You do not have permission to use this command."
     else:
         # Reset the word count data by clearing the JSON file
@@ -285,19 +247,17 @@ async def reset_confirm(interaction: discord.Interaction):
     reset_confirm_embed = discord.Embed(
         title="Reset Confirmation",
         description=message,
-        color=discord.Colour.blue()
+        color=discord.Colour.blurple()
     )
-    
-    embed.set_footer(text=f"Made by mal023")
 
     # Send the message as an ephemeral response
-    await interaction.response.send_message(embed=reset_confirm_embed)
+    await ctx.respond(embed=reset_confirm_embed)
 
 
-@bot.tree.command(name="setwordc")
-async def setwordc_command(interaction: discord.Interaction, user: discord.User = None, count: int = None):
+@bot.slash_command(name="setwordc", description="Set a user's word count (admin only)")
+async def setwordc_command(ctx, user: discord.User, count: Option(int, description="Enter the users wordcount", required=True)): # type: ignore
     # Check if the user invoking the command has Administrator permission
-    if not interaction.user.guild_permissions.administrator:
+    if not ctx.user.guild_permissions.administrator:
         message = "You do not have permission to use this command."
     else:
         # Check if the user ID or count is empty
@@ -321,13 +281,11 @@ async def setwordc_command(interaction: discord.Interaction, user: discord.User 
             message = discord.Embed(
                 title="Word Count Set Confirmation",
                 description=f"{user.name}'s word count has been set to {count}.",
-                color=discord.Colour.blue()
+                color=discord.Colour.blurple()
             )
-
-    embed.set_footer(text=f"Made by mal023")
     
     # Send the message as an ephemeral response
-    await interaction.response.send_message(embed=message)
+    await ctx.respond(embed=message)
 
 
 bot.run(bot_token)
